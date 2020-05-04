c7df7860170cf1b176b3d0a07c8504189e575fbd
Camel Aissani <camel.aissani@nuageprive.fr>
2020-05-04 18:36:20 +0200

    replaced http.request by axios

--- backend/managers/emailmanager.js
+++ backend/managers/emailmanager.js
@@ -1,81 +1,40 @@
 const moment = require('moment');
-const http = require('http');
-const { URL } = require('url');
+const axios = require('axios');
 const logger = require('winston');
 const config = require('../../config');
 
-function sendEmail(messages) {
-    return Promise.all(messages.map((message) => {
-        return new Promise((resolve, reject) => {
-            const postData = JSON.stringify({
-                'document': message.document,
-                'id': message.tenantId,
-                'term': message.term
-            });
-            const postUrl = new URL(config.EMAILER_URL);
-            const postOption = {
-                hostname: postUrl.hostname,
-                port: postUrl.port,
-                path: postUrl.pathname,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Length': Buffer.byteLength(postData)
-                }
-            };
-            const req = http.request(postOption);
-            req.on('response', res => {
-                res.setEncoding('utf8');
-                let body = '';
-                res.on('data', chunk => {
-                    body += chunk;
-                });
-                res.on('end', () => {
-                    if (res.statusCode>299) {
-                        logger.error(`POST ${config.EMAILER_URL} ${res.statusCode}`);
-                        logger.error(`data sent: ${postData}`);
-                        if (res.body) {
-                            logger.error(`response: ${res.body}`);
-                            reject(res.body);
-                            return;
-                        }
-                        if (config.demoMode) {
-                            logger.info('email status fallback workflow activated in demo mode');
-                            const result = Object.assign({}, message);
-                            result.error = 'demo mode, emails cannot be sent';
-                            resolve([result]);
-                        } else {
-                            reject(`${res.statusCode} ${res.statusMessage}`);
-                        }
-                        return;
-                    }
-                    logger.info(`POST ${config.EMAILER_URL} ${res.statusCode}`);
-                    logger.debug(`data sent: ${postData}`);
-                    logger.debug(`response: ${body}`);
-                    resolve(JSON.parse(body));
-                });
-            });
-            req.on('error', err => {
-                logger.error(`POST ${config.EMAILER_URL} failed`);
-                logger.error(`data sent: ${postData}`);
-                logger.error(err);
-                if (config.demoMode) {
-                    logger.info('email status fallback workflow activated in demo mode');
-                    const result = Object.assign({}, message);
-                    result.error = 'demo mode, mail cannot be sent';
-                    resolve([result]);
-                } else {
-                    reject(err);
-                }
-            });
-            req.write(postData);
-            req.end();
-        });
-    }));
-}
+
+const sendEmail = async message => {
+    const postData = {
+        'document': message.document,
+        'id': message.tenantId,
+        'term': message.term
+    };
+
+    try {
+        const response = await axios.post(config.EMAILER_URL, postData);
+
+        logger.info(`POST ${config.EMAILER_URL} ${response.status}`);
+        logger.debug(`data sent: ${postData}`);
+        logger.debug(`response: ${response.data}`);
+        return response.data;
+    } catch (error) {
+        logger.error(`POST ${config.EMAILER_URL} failed`);
+        logger.error(`data sent: ${postData}`);
+        logger.error(error);
+        if (config.demoMode) {
+            logger.info('email status fallback workflow activated in demo mode');
+            const result = { ...message };
+            result.error = 'demo mode, mail cannot be sent';
+            return [ result ];
+        } else {
+            throw error;
+        }
+    }
+};
 
 module.exports = {
-    send(req, res) {
+    async send(req, res) {
         const body = req.body;
         const document = body.document;
         const term = moment(`${body.year}/${body.month}/01`, 'YYYY/MM/DD').format('YYYYMMDDHH');
@@ -86,17 +45,16 @@ module.exports = {
                 term
             };
         });
-        sendEmail(messages)
-            .then(status => {
-                const results = status.reduce((acc, result) => {
-                    acc.push(...result);
-                    return acc;
-                }, []);
-                res.status(200).json(results);
-            })
-            .catch(err => {
-                res.status(500).send(err);
-                logger.error(err);
-            });
+        const status = await Promise.all(messages.map(message => sendEmail(message)));
+        try {
+            const results = status.reduce((acc, result) => {
+                acc.push(...result);
+                return acc;
+            }, []);
+            res.json(results);
+        } catch(err) {
+            logger.error(err);
+            res.status(500).send(err);
+        }
     }
 };
